5ff9945342e2cf759f3e4fe7942aaa6609492479
Lauren Zou <zou.lauren@gmail.com>
2015-01-18 00:34:02 -0500

    Envelope now automagically resizes itself based on browser size. Envelope also shifts down as letter is being scrolled through. Added title.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
+var envelope_offset_bottom = 20;
+var title_offset_bottom;
+
 $(document).ready(function() {
   envelope_setup();
   $(window).resize(function() {
@@ -5,31 +8,54 @@ $(document).ready(function() {
   });
 
   $(window).scroll(function() {
-    envelope_open();
+    var scroll_top = $(window).scrollTop();
+    envelope_shift(scroll_top);
+    title_shift(scroll_top);
+    envelope_open(scroll_top);
   })
 });
 
 function envelope_setup() {
   var envelope = $('#envelope');
+  var title = $('#title');
+  var title_line_height = parseInt(title.css('height'));
   var envelope_width = envelope.width();
+  var envelope_height = Math.floor($('body').height() - envelope_offset_bottom*2 - title_line_height*2);
+  if (envelope_height % 2 !== 0) {
+    // Make sure the height is even
+    envelope_height -= 1;
+  }
+  envelope.css('height', envelope_height);
+  $('#envelope-details-wrapper').css('height', envelope_height);
+
+  // Place the title
+  title_offset_bottom = envelope_height + parseInt(envelope.css('bottom'));
+  title.css('bottom', title_offset_bottom + 'px');
 
   // Adjust envelope details
   var envelope_details = $('#envelope-details');
   envelope_details.css({
     'border-left-width' : envelope_width/2 + 'px',
-    'border-right-width' : envelope_width/2 + 'px'
+    'border-right-width' : envelope_width/2 + 'px',
+    'border-top-width' : envelope_height/2 + 'px',
+    'border-bottom-width' : envelope_height/2 + 'px'
   });
 
   // Adjust envelope flap
   var envelope_flap_outside = $('#envelope-flap-outside');
   envelope_flap_outside.css({
     'border-left-width' : envelope_width/2 + 'px',
-    'border-right-width' : envelope_width/2 + 'px'
+    'border-right-width' : envelope_width/2 + 'px',
+    'border-top-width' : envelope_height/2 + 'px',
+    'border-bottom-width' : envelope_height/2 + 'px'
   });
   var envelope_flap_inside = $('#envelope-flap-inside');
   envelope_flap_inside.css({
+    'top' : -envelope_height + 'px',
     'border-left-width' : envelope_width/2 + 'px',
-    'border-right-width' : envelope_width/2 + 'px'
+    'border-right-width' : envelope_width/2 + 'px',
+    'border-top-width' : envelope_height/2 + 'px',
+    'border-bottom-width' : envelope_height/2 + 'px'
   });
 
   // Offset top for letter
@@ -37,8 +63,28 @@ function envelope_setup() {
   letter.css('margin-top', $('body').height() - envelope.height()/2);
 }
 
-function envelope_open() {
-  var scroll_top = $(window).scrollTop();
+function envelope_shift(scroll_top) {
+  if (scroll_top < 180) {
+    $('#envelope').css('bottom', envelope_offset_bottom + 'px');
+    $('#envelope-details-wrapper').css('bottom', envelope_offset_bottom + 'px');
+    return;
+  }
+
+  $('#envelope').css('bottom', (envelope_offset_bottom - scroll_top + 180) + 'px');
+  $('#envelope-details-wrapper').css('bottom', (envelope_offset_bottom - scroll_top + 180) + 'px');
+}
+
+function title_shift(scroll_top) {
+  if (scroll_top > 60) {
+    $('#title').hide();
+    return;
+  }
+
+  $('#title').show();
+  $('#title').css('bottom', (title_offset_bottom - scroll_top) + 'px');
+}
+
+function envelope_open(scroll_top) {
   if (scroll_top > 180 || scroll_top < 0) {
     return;
   }
